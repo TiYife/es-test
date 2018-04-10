6ef3c1c0be2d3108172d762bfdc298c0758a84e5
tiyife <13051087618@163.com>
2018-04-10 18:23:44 +0800

    简单搜索

---
+++ src/main/java/es/controller/SearchController.java
@@ -0,0 +1,59 @@
+package es.controller;
+
+import com.google.gson.Gson;
+import es.entity.esEntity.DocEntity;
+import es.service.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.net.URLEncoder;
+import java.util.List;
+
+/**
+ * Created by TYF on 2018/4/8.
+ */
+@Controller
+public class SearchController {
+
+    @Autowired
+    SearchService searchService;
+
+    @RequestMapping("/toSimpleSearch")
+    @ResponseBody
+    public String toSimplySearch(@RequestParam("type")String type,
+                               @RequestParam("keyword") String keyword,
+                               Model model){
+        String attr, content;
+        if (type == "标题") attr = "caseName";
+        else attr = "content";
+//        List<DocEntity> list = searchService.searchLaw(0,10,attr,keyword);
+//        model.addAttribute("attr",attr);
+//        model.addAttribute("content",keyword);
+//        return "searchResult";
+        return "simpleSearchResult?attr=" + attr + "&keyword=" + URLEncoder.encode(keyword);
+    }
+
+    @RequestMapping("/simpleSearchResult")
+    public String simpleSearchResult(@RequestParam("attr")String attr,
+                                     @RequestParam("keyword") String keyword,
+                                     Model model){
+        model.addAttribute("attr",attr);
+        model.addAttribute("keyword",keyword);
+        return "search-result";
+    }
+
+
+    @RequestMapping("/simpleSearch")
+    @ResponseBody
+    public String simpleSearch(@RequestParam("attr")String attr,
+                               @RequestParam("keyword") String keyword){
+        List<DocEntity> list = searchService.searchLaw(0,10,attr,keyword);
+        Gson gson = new Gson();
+        return gson.toJson(list);
+    }
+}
