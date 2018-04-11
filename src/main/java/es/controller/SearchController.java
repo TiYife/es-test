03f941a93544f976c16c47ccb5d2c0c8bc8ff552
gaoye <17801006236@163.com>
2018-04-11 12:03:35 +0800

    数据显示bug

--- src/main/java/es/controller/SearchController.java
+++ src/main/java/es/controller/SearchController.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import es.entity.esEntity.DocEntity;
 import es.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
